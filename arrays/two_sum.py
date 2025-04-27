c29512d441ecbf67e9e68ac02d9ca348747b82ba
bisma-Git <bismaxcode@gmail.com>
2025-04-27 06:37:37 -0700

    new

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,3 +1,13 @@
-print('hello')
-print('bisma')
-print('sajid')
+'''Given an array of integers nums and an integer target, return indices of the two numbers
+ such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same
+element twice.
+You can return the answer in any order.'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen={}
+        for i,j in enumerate(nums):
+            dif=target-j
+            if dif  in seen:
+                return [seen[dif],i]
+            seen[j]=i
